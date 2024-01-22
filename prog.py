e0f8a313bafe2b602aa5195cb9d4730889746329
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 18:36:00 +0800

    first list working

---
+++ prog.py
@@ -0,0 +1,9 @@
+first_list = [10, 53, 70, 90, 37]
+second_list = [56, 54, 33, 11, 97]
+product_list = []
+
+for number in first_list:
+    if number%2 != 0:
+        product_list.append(number)
+
+print(product_list)
